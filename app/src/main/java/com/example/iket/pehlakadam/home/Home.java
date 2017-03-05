3aa50976221e3886fbc452954795a48dc2ec6ccc
iket0512 <iket.ag24@gmail.com>
2017-03-05 13:11:27 +0530

    basic fab added

--- app/src/main/java/com/example/iket/pehlakadam/home/Home.java
+++ app/src/main/java/com/example/iket/pehlakadam/home/Home.java
@@ -1,5 +1,6 @@
 package com.example.iket.pehlakadam.home;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +15,7 @@ import android.widget.Toast;
 
 import com.example.iket.pehlakadam.R;
 import com.example.iket.pehlakadam.helper.SharedPrefs;
-import com.joaquimley.faboptions.FabOptions;
+
 
 public class Home extends AppCompatActivity {
 
@@ -27,12 +28,6 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        final FabOptions fabOptions = (FabOptions) findViewById(R.id.fab_options);
-
-
-
-
         sharedPrefs=new SharedPrefs(this);
         sharedPrefs.setFirstTimeLaunch(false);
 
@@ -41,37 +36,6 @@ public class Home extends AppCompatActivity {
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewpager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewpager);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_float, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.faboptions_favorite:
-                Log.d("Res","1");
-                Toast.makeText(this, "Favourite", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.faboptions_share:
-                Log.d("Res","1");
-                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.faboptions_download:
-                Log.d("Res","1");
-                Toast.makeText(this, "Download", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.faboptions_textsms:
-                Log.d("Res","1");
-                Toast.makeText(this, "Text", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
 }
